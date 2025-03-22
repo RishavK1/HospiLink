336e118b89cd96a795c150576d2ac63e62a9a84c
Rishav <rishavkamboj75@gmail.com>
2025-03-22 20:59:08 +0530

    New change in admin pannel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login'
+// import { ToastContainer, toast } from "react-toastify";
+import { Toaster, toast } from "react-hot-toast";
+import { AdminContext } from './context/AdminContext';
+
+const {aToken} = useContext(AdminContext)
+
 
 const App = () => {
-  return (
+  return aToken ? (
     <div >
-      <Login/>
+      <Toaster />
     </div>
+  ) : (
+      <>
+      <Login />
+      <Toaster />
+      </>
   )
 }
 
