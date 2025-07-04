6c091215a0df6e66923d39ca286b3ac83c8c8a79
RishavK1 <rishavkamboj75@gmail.com>
2025-07-04 23:47:30 +0530

    Update axios to version 1.10.0 and implement doctor fetching functionality in AdminContext

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,23 +1,51 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 
 export const AdminContext = createContext();
 const AdminContextProvider = (props) => {
-     
-    const [aToken, setAToken] = useState(localStorage.getItem("aToken")?localStorage.getItem("aToken"):"");
+  const [aToken, setAToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
+  const [doctors, setDoctors] = useState([]);
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    const value = {
-        aToken,
-        setAToken,
-        backendUrl,
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-doctors",
+        {},
+        {
+          headers: {
+            aToken,
+          },
+        }
+      );
+
+      if(data.success) {
+      setDoctors(data.doctors);
+      console.log("Doctors fetched successfully:", data.doctors);
+      }
+      else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error("Error fetching doctors:", error);
     }
-    
-    return (
-        <AdminContext.Provider value={value}>
-            {props.children}
-        </AdminContext.Provider>
-    )
-}
+  };
+  const value = {
+    aToken,
+    setAToken,
+    backendUrl,
+    doctors,
+    getAllDoctors,};
 
-export default AdminContextProvider
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminContextProvider;
